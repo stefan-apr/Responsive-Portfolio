60210f045afcc6a77237e210ba05373ebcf6417c
sapreut <stefan_apr@hotmail.com>
2019-04-29 20:34:39 -0700

    Changed some stuff

--- assets/js/contact-logic.js
+++ assets/js/contact-logic.js
@@ -12,28 +12,47 @@ $(document).ready(function() {
     var database = firebase.database();
 
     $(".submitButton").click(function() {
+        event.preventDefault();
+        $("#user-warning").css("color", "darkred");
+
         let submittedName = $("#name").val();
         let submittedEmail = $("#email").val();
         let submittedMessage = $("#message").val();
 
         if(submittedName === null || submittedName === "" || submittedEmail === null || submittedEmail === "" || submittedMessage === null || submittedMessage === "") {
-            console.log("The user didn't input every field.");
-            break;
+            $("#user-warning").text("All fields are required to submit.");
+            $("#user-warning").attr("class", "visible");
+            fadeOut();
+            return;
         } else {
             if(!submittedEmail.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/)) {
-                console.log("I don't think that's a valid email address.");
-                break;
+                $("#user-warning").text("The email doesn't seem to be a valid format.");
+                $("#user-warning").attr("class", "visible");
+                fadeOut();
+                return;
             } 
             if(!submittedName.match(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u)) {
-                console.log("This name can't be saved.");
-                break;
+                $("#user-warning").text("This name uses characters that can't be stored in my database.");
+                $("#user-warning").attr("class", "visible");
+                fadeOut();
+                return;
             }
+            $("#user-warning").text("Inquiry submitted succesfully!");
+            $("#user-warning").css("color", "green");
+            $("#user-warning").attr("class", "visible");
+            fadeOut();
         }
 
-        database.ref("/inquiries").set({
+        database.ref("/inquiries").push({
             name: submittedName,
             email: submittedEmail,
             message: submittedMessage
         });
     });
+
+    function fadeOut() {
+        setTimeout(function(){ 
+            $("#user-warning").attr("class", "hidden");
+        }, 2000);
+    }
 });
